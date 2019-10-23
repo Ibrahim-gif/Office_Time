40d57ad8f6640ddb9b62f64a286e9857049275b0
Ibrahim-gif <56799071+Ibrahim-gif@users.noreply.github.com>
2019-10-24 01:12:31 +0530

    An E-Commerce website titled Office Time

---
+++ app/etc/env.php
@@ -0,0 +1,51 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_tq2qf'
+    ],
+    'crypt' => [
+        'key' => '6f50d6001a7ca4ae8d823541d2011bfa'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento',
+                'username' => 'root',
+                'password' => '',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 0,
+        'layout' => 0,
+        'block_html' => 0,
+        'collections' => 0,
+        'reflection' => 0,
+        'db_ddl' => 0,
+        'compiled_config' => 0,
+        'eav' => 0,
+        'customer_notification' => 0,
+        'config_integration' => 0,
+        'config_integration_api' => 0,
+        'full_page' => 0,
+        'config_webservice' => 0,
+        'translate' => 0,
+        'vertex' => 0
+    ],
+    'install' => [
+        'date' => 'Tue, 05 Feb 2019 11:32:52 +0000'
+    ]
+];
